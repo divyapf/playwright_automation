9090bc8a9a7c87f3a19489a558ac21cb613c6f17
divyapf <divyafrancis2007@gmail.com>
2026-06-26 14:15:40 +0200

    added parameterised tests for multiple invalide credentials

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,6 @@
 # tests/test_login.py
 # These tests check the login page of SauceDemo
-
+import pytest
 from pages.login_page import LoginPage
 
 class TestLogin:
@@ -12,7 +12,7 @@ class TestLogin:
         login = LoginPage(login_page)
         login.login("standard_user", "secret_sauce")
 
-        # Check we are now on the inventory/products page
+        # Checking if we are now on the inventory/products page
         assert "inventory" in login_page.url
 
     def test_wrong_password_shows_error(self, login_page):
@@ -21,5 +21,16 @@ class TestLogin:
         """
         login = LoginPage(login_page)
         login.login("standard_user", "wrong_password")
+        assert login.is_error_visible()
 
+        
+    @pytest.mark.parametrize("username, password",
+                              
+       [ ("locked_out_user", "secret_sauce"),
+        ("wrong_user",      "secret_sauce"),
+        ("standard_user",   "wrong_pass"),])
+    
+    def test_invalid_credentials(self, login_page, username, password):
+        login = LoginPage(login_page)
+        login.login(username, password)
         assert login.is_error_visible()
